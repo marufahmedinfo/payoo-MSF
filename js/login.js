e854ade02102431889b601c61b5f2163c1772745
marufahmedinfo <marufahmedinfos@gmail.com>
2024-09-19 16:31:31 +0600

    added simple login handel & prevent default

---
+++ js/login.js
@@ -0,0 +1,12 @@
+document.getElementById('login-btn').addEventListener('click', function(event){
+    event.preventDefault();
+    const phoneNumber = document.getElementById('phone-number').value;
+    const pinNumber = document.getElementById('pin-number').value;
+    // console.log(phoneNumber, pinNumber);
+    if(phoneNumber === "01320316967" && pinNumber === "1234"){
+        console.log('right you');
+        window.location.href = "/home.html"
+    }else{
+        alert("Wrong Phone Number & Pin Number")
+    }
+})
